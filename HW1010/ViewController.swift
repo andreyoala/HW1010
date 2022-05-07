4794f79a8f1ddad334f2d22682a6124ecade667f
Andrey <andreyoala@icloud.com>
2022-05-07 12:42:23 +0300

    Cleaned the comments from code.

--- HW1010/ViewController.swift
+++ HW1010/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  HW1010
-//
-//  Created by Andrey Oala on 07.05.2022.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     func setupView() {
